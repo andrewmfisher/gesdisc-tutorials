eba4f5a509e25cf9b69c5c963e6419d4682e20af
jschap1 <17228121+jschap1@users.noreply.github.com>
2026-08-31 16:17:20 -0400

    created skeleton main.py

---
+++ conference-presentations/agu-2026/main.py
@@ -0,0 +1,16 @@
+# Main script for benchmarking data access methods
+#
+# 8/31/2026 JRS
+# Methods are defined in separate files
+
+import numpy
+
+# Load functions from separate function files
+
+# Call each to get data for the domain of interest
+
+# Track computational demands for each method
+
+# Plot method vs. computational requirements
+
+
