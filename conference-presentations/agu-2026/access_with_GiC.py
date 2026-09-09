601e5d8e5e14386ebc88c597381d296dc355b677
jschap1 <17228121+jschap1@users.noreply.github.com>
2026-09-09 12:50:58 -0400

    updates to access scripts

--- conference-presentations/agu-2026/access_with_GiC.py
+++ conference-presentations/agu-2026/access_with_GiC.py
@@ -21,3 +21,34 @@ def get_GiC():
     print("starting get_GiC")
     print("this is a placeholder function pending development")
     data = 1
+
+
+#     Dataset: NLDAS_FORA0125_H, version 2.0
+
+# Location:
+# Latitude: 38.9517
+# Longitude: -92.3341
+
+# Date range:
+# 2024-01-01T00:00:00/2024-01-10T23:00:00
+
+# Air temperature request parameters:
+# data=NLDAS_FORA0125_H_2_0_Tair
+# location=[38.9517,-92.3341]
+# time=2024-01-01T00:00:00/2024-01-10T23:00:00
+# version=2.0
+
+# Precipitation request parameters:
+# data=NLDAS_FORA0125_H_2_0_Rainf
+# location=[38.9517,-92.3341]
+# time=2024-01-01T00:00:00/2024-01-10T23:00:00
+# version=2.0
+
+# Works: https://api.giovanni.earthdata.nasa.gov/timeseries?data=NLDAS_FORA0125_H_2_0_Rainf&location=[38.99,-76.85]&time=2000-06-01T00:00:00/2000-06-01T07:30:00&version=2.0
+# Works: https://api.giovanni.earthdata.nasa.gov/timeseries?data=NLDAS_FORA0125_H_2_0_Rainf&location=[38.9517,-92.3341]&time=2000-06-01T00:00:00/2000-06-01T07:30:00&version=2.0
+
+# https://api.giovanni.earthdata.nasa.gov/timeseries?data=NLDAS_FORA0125_H_2_0_Rainf&location=[38.9517,-92.3341]&time=2024-01-01T00:00:00/2024-01-10T23:00:00&version=2.0
+
+curl -H "authorization:Bearer ${bearer_token}" -X --url "https://api.giovanni.earthdata.nasa.gov/timeseries?data=NLDAS_FORA0125_H_2_0_Rainf&location=[38.9517,-92.3341]&time=2024-01-01T00:00:00/2024-01-10T23:00:00&version=2.0" -o "test.csv"
+
+
